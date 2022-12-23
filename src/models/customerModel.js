b3bdfa593e8cc805f61e70fb5cbdda64722d3205
nickgautam <er.nishantgautam0011@gmail.com>
2022-12-24 02:21:28 +0530

    Assignment Completed

--- src/models/customerModel.js
+++ src/models/customerModel.js
@@ -5,9 +5,9 @@ const customerSchema = new mongoose.Schema({
     firstName: { type: String, required: true, trim: true },
     lastName: { type: String, required: true, trim: true },
     mobileNumber: { type: String, required: true, unique: true, trim: true },
-    DOB: { type: Date, required: true },
+    DOB: { type: Date, required: true, trim: true },
     emailID: { type: String, required: true, unique: true, trim: true },
-    address: { type: String, required: true },
+    address: { type: String, required: true, trim: true },
     customerID: { type: String, required: true, unique: true, trim: true },
     status: { type: String, enum: ["ACTIVE", "INACTIVE"], default: "ACTIVE", trim: true },
     isDeleted: { type: Boolean, default: false, trim: true }
